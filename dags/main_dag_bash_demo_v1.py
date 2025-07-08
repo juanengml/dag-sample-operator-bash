8ed0278ac408a9fc83464424005b2ae3a86c86c5
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2025-07-08 15:16:12 -0300

    Update main_dag_bash_demo_v1.py

--- dags/main_dag_bash_demo_v1.py
+++ dags/main_dag_bash_demo_v1.py
@@ -1,57 +1,56 @@
-## lista de demos
-## https://gist.github.com/sakethramanujam/7c21497dbd11d9b7d93b57c437d2c4dd
-
 from datetime import timedelta
-import pandas as pd 
+import pandas as pd
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
-#from airflow.operators.papermill_operator import PapermillOperator
 
-
-args = {
+default_args = {
     'owner': 'me',
+    'time': 'eng_dados',
 }
 
 with DAG(
-    dag_id='abobora_dag',
-    default_args=args,
+    dag_id='elt-pipeline-iris',
+    default_args=default_args,
     schedule_interval='@hourly',
     start_date=days_ago(2),
     dagrun_timeout=timedelta(minutes=60),
-    tags=['etl', 'BashOperator'],
+    tags=['etl', 'bash', 'python'],
     params={"example_key": "example_value"},
 ) as dag:
 
     def extracao():
-       print("Hello Airflow Using a Python Operator!\nextracao")
-       df = pd.read_csv("https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv")
-       df.to_csv("base.csv")  
+        print("Iniciando extração de dados...")
+        df = pd.read_csv(
+            "https://gist.githubusercontent.com/netj/8836201/raw/"
+            "6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
+        )
+        df.to_csv("/tmp/base.csv", index=False)
+        print("Extração concluída e salva em /tmp/base.csv")
 
     def transformacao():
-       print("Hello Airflow Using a Python Operator!\ntransformacao")
-       df = pd.read_csv("base.csv")
-       df[["sepal.length","sepal.width","petal.length","petal.width"]].to_csv("base_limpa.csv")
+        print("Iniciando transformação de dados...")
+        df = pd.read_csv("/tmp/base.csv")
+        df[["sepal.length", "sepal.width", "petal.length", "petal.width"]].to_csv("/tmp/base_limpa.csv", index=False)
+        print("Transformação concluída e salva em /tmp/base_limpa.csv")
 
-    def load():
-       print("Hello Airflow Using a Python Operator!\nLOAD")
+    extract = PythonOperator(
+        task_id='extracao',
+        python_callable=extracao
+    )
 
+    transform = PythonOperator(
+        task_id='transformacao',
+        python_callable=transformacao
+    )
 
-    extract = PythonOperator(task_id='extract',
-                             python_callable=extracao)
-    
-    trans = PythonOperator(task_id='task-trans',
-                             python_callable=transformacao)
- 
     load = BashOperator(
         task_id='carga',
-        bash_command='cat base_limpa.csv',
+        bash_command='cat /tmp/base_limpa.csv',
     )
-      
-    # test   
 
-    extract >> trans >> load 
+    extract >> transform >> load
 
 if __name__ == "__main__":
     dag.cli()
